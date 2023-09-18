2cf8d07a1e9b1fd0203f0902f57443abd8e4746b
yuyaasai <74097794+yuyaasai@users.noreply.github.com>
2023-09-19 00:19:40 +0900

    ESLint + Prettier の導入

---
+++ TsMemo/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution")
+
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        es2022: true
+    },
+    extends: ["standard-with-typescript", "prettier"],
+    overrides: [],
+    plugins: ["@typescript-eslint"],
+    parserOptions: {
+        ecmaVersion: "latest",
+        parser: "@typescript-eslint/parser",
+        project: "./tsconfig.eslint.json",
+        sourceType: "module",
+        tsconfigRootDir: __dirname
+    }
+}
