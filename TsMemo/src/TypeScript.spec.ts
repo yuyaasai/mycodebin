ebd626666b7edc325bbf09c840417478a7e7510d
yuyaasai <74097794+yuyaasai@users.noreply.github.com>
2023-10-11 21:21:34 +0900

    TypeScript.spec.ts REBASE

--- TsMemo/src/TypeScript.spec.ts
+++ TsMemo/src/TypeScript.spec.ts
@@ -1,5 +1,15 @@
 import { describe, it, expect } from "vitest"
 
+// [リリースノート] https://www.typescriptlang.org/docs/handbook/release-notes/typescript-1-1.html
+
+describe("TypeScript 2.2", () => {
+    it("object型", () => {
+        // object 型はプリミティブ型 (number, string, boolean, bigint, symbol, null, undefined) 以外の型を表す
+        const obj: object = { a: 1 }
+        expect(obj).toEqual({ a: 1 })
+    })
+})
+
 describe("TypeScript 3.7", () => {
     it("asserts", () => {
         function assertIsString(value: unknown): asserts value is string {
